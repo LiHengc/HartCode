fadc390fe450c087a5424ed33786f7cc3ea70887
harthali <138868082@qq.com>
2019-09-18 13:42:41 +0800

    update

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once "vendor/autoload.php";
-use Hart\QrCode\HartQrcode;
-$hartqrCode = new HartQrcode('http://www.baidu.com','v1.png',true);
-$hartqrCode->create_qrcode();
-$hartqrCode->create_bg_qrcode();
-$hartqrCode->get_bg_qrcode();
